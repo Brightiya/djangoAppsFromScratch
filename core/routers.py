585452010b6ecd92e81d047cddafad6fe2684974
Brightiya <bightiyahen@gmail.com>
2023-11-26 14:17:28 +0100

    initial commit

---
+++ core/routers.py
@@ -0,0 +1,17 @@
+from rest_framework import routers
+from .user.viewSets import UserViewSet
+from .auth.viewsets.register import RegisterViewSet
+from .auth.viewsets.login import  LoginViewSet
+from .auth.viewsets.refresh import  RefreshViewSet
+router = routers.SimpleRouter()
+
+router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+
+urlpatterns = [
+
+    *router.urls,
+]
